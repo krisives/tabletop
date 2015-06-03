d623117a3bb7aa3ba95ab4a92ff866ebecb22511
Kristopher Ives <kristopher.ives@gmail.com>
2015-06-02 18:04:00 -0700

    Added help and TODO for the /roll command.

--- commands/roll.js
+++ commands/roll.js
@@ -1,9 +1,13 @@
 
 function roll(app, args) {
+	// TODO this needs to use a synchronized random
 	var x = 1 + (~~Math.round(Math.random() * 5));
 	app.log.print("Rolled a " + x);
 }
 
-roll.help = "Roll a die";
+roll.summary = "Roll a die";
+roll.usage = "/roll";
+roll.help = "Roll a die randomly. This is a six sided die";
+
 
 module.exports = roll;
